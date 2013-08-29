39f633ad943b7f3cf65f56528bd6ecca63aff32b
happycoda <ypworkspace@gmail.com>
2013-08-29 12:01:06 +0400

    jquery 2.0.3 & normalize.css download added

--- script.py
+++ script.py
@@ -4,7 +4,7 @@
 
 #!/usr/bin/env python
 
-import os
+import os, urllib2
 
 # dirs = ['root', 'root/css', 'root/js', 'root/i']
 # files = {'css': , 'js': }
@@ -27,27 +27,47 @@ def make_dirs(m):
 
 		for dir_name in nodes:
 			val = nodes[dir_name]
-			os.makedirs(root + '/' + dir_name)
-			if type(val) == dict:
-				make_dirs(val)
-			elif type(val) == list:
-				for fname in val:
-					make_file(root + '/' + dir_name + '/' + fname)
+			dir_path = root + '/' + dir_name
+
+			if not os.path.exists(dir_path):
+				os.makedirs(dir_path)
+				if type(val) == dict:
+					make_dirs(val)
+				elif type(val) == list:
+					for fname in val:
+
+						if fname == 'normalize.css':
+							normalize_url = urllib2.urlopen('http://necolas.github.io/normalize.css/2.1.3/normalize.css');
+							normalize_data = normalize_url.read();
+							make_file(dir_path + '/' + fname, normalize_data)
+						elif fname == 'jquery.js':
+							jquery_url = urllib2.urlopen('http://code.jquery.com/jquery-2.0.3.min.js');
+							jquery_data = jquery_url.read();
+							make_file(dir_path + '/' + fname, jquery_data)
+						else:
+							make_file(dir_path + '/' + fname)	
 
 	if m['root_files']:
 		root_files = m['root_files']
 
 		for rfname in root_files:
-			make_file(root + '/' + rfname)
+			file_path = root + '/' + rfname
+			if not os.path.exists(file_path):
+				make_file(file_path)
 	
 
 	
 
+
 	
 
 
-def make_file(path):
+def make_file(path, data=None):
 	new_file = open(path, 'w')
+
+	if not data == None:
+		new_file.write(data)
+
 	new_file.close()
 		
 make_dirs(dir_file_map)
